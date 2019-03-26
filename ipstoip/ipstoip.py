9aaaecae616a773d1e87192538c6eb7e7c793b0e
legoc <472577989@qq.com>
2019-03-26 23:23:15 +0800

    ipstoip_script

---
+++ ipstoip/ipstoip.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# __author__ = "lego"
+# requirements: IPy
+import IPy
+import sys
+
+msg = '''
+Usage: python ipstoip.py ips.txt
+'''
+
+f=open('ip',"w+")
+
+if len(sys.argv) < 2:
+    print msg
+    exit()
+for ips in open("ips.txt"):
+	ip = IPy.IP(ips)
+	for x in ip:
+   	    f.writelines(str(x)+'\n')
